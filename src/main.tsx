59897d879943debc49d6eac0c4f76521bcf8bed6
krllms-wq <krllms@gmail.com>
2025-08-22 23:07:33 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/Login'
+import Login2 from './pages/Login2'
 
 function Home() {
-  return <div style={{padding:24}}>
-    <h1>CapTablePro</h1>
-    <p>Главная страница. Перейти на <a href="/login">/login</a></p>
-  </div>
+  return (
+    <div style={{padding:24, fontFamily:'system-ui, sans-serif'}}>
+      <h1>CapTablePro</h1>
+      <p>
+        Главная страница. Перейти на{' '}
+        <a href="/login">/login</a> или <a href="/login2">/login2</a>
+      </p>
+    </div>
+  )
 }
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/login', element: <Login /> },
+  { path: '/login2', element: <Login2 /> },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
